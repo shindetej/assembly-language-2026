f4d13b87a185dcf76f6663f0eb998cceb7b450f1
shindetej <shindetejasm@gmail.com>
2026-08-05 00:58:58 +0530

    Day 5 Local static [C] assembly pending

---
+++ day5/30_local_static.c
@@ -0,0 +1,32 @@
+int main()
+{
+    int iCounter;
+
+    printf("\nCalling fun1\n");
+    for(iCounter = 0; iCounter < 3; iCounter++)
+        fun1();
+
+    printf("\nCalling fun2\n");
+    for(iCounter = 0; iCounter < 3; iCounter++)
+        fun2();
+
+    exit(0);
+}
+
+void fun1()
+{
+    int iNo = 10;
+
+    iNo++;
+
+    printf("In fun1, iNo = %d\n", iNo);
+}
+
+void fun2()
+{
+    static int iNo = 10;
+
+    iNo++;
+
+    printf("In fun2, iNo = %d\n", iNo);
+}
