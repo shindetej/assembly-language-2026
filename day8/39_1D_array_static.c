9fb47f41c3b80164d6645df327bfeaf35e9f7093
shindetej <shindetejasm@gmail.com>
2026-08-24 00:03:30 +0530

    day-8 : topic 1D array in  assembly only c codes

---
+++ day8/39_1D_array_static.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10
+
+int main()
+{
+	int iMax;
+	int iCounter;
+	int arr[MAX];
+
+	printf("Enter value of n(< %d):\t", MAX);
+	scanf("%d", &iMax);
+
+	for(iCounter = 0; iCounter < iMax; iCounter++)
+	{
+		printf("Enter %d value:\t", iCounter);
+		scanf("%d", &arr[iCounter]);
+	}
+
+	printf("Entered elements are:\n");
+
+	for(iCounter = 0; iCounter < iMax; iCounter++)
+		printf("%d value is:\t%d\n", iCounter, arr[iCounter]);
+
+	exit(0);
+}
